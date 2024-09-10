3390022748bb02109c1a201592f5f4efd384ac07
pawarasenarathna <pawarasenarathna@gmail.com>
2024-09-10 11:32:05 +0530

    Change some Error

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
 
-import AdminMenu from './Pages/Admin/AdminMenu';
-import UpdateUser from './Pages/Admin/UpdateUser';
+// import AdminMenu from './Pages/Admin/AdminMenu';
+// import UpdateUser from './Pages/Admin/UpdateUser';
 
-import Gallery from './Pages/Gallery';
+// import Gallery from './Pages/Gallery';
 import HomePage from './Pages/HomePage';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import Resevation from './Pages/Resevation';
-import Staff from './Pages/Staff';
-import OnlineOrder from './Pages/OnlineOrder';
-import CustomerFeedback from './Pages/CustomerFeedback';
-import Payment from './Pages/Payment';
-import EditMenu from './Pages/EditMenu.Js';
-import Report from './Pages/AdminReport';
+// import Login from './Pages/Login';
+// import Register from './Pages/Register';
+// import Resevation from './Pages/Resevation';
+// import Staff from './Pages/Staff';
+// import OnlineOrder from './Pages/OnlineOrder';
+// import CustomerFeedback from './Pages/CustomerFeedback';
+// import Payment from './Pages/Payment';
+// import EditMenu from './Pages/EditMenu.Js';
+// import Report from './Pages/AdminReport';
 
-import Navigation from './Pages/Navigation';
-import Contact from './Pages/Contact';
-import AdminReport from './Pages/AdminReport';
-import AdminPage from './Pages/Admin/Admin';
+// import Navigation from './Pages/Navigation';
+// import Contact from './Pages/Contact';
+// import AdminReport from './Pages/AdminReport';
+// import AdminPage from './Pages/Admin/Admin';
 
 
 
@@ -32,18 +32,18 @@ function App() {
   return (
       <div className="App">
        <HomePage/> 
-       <Login/>
-       <Resevation/>
-      //  <Register/>
-      //  <Staff/>
-      <OnlineOrder/>
-      <CustomerFeedback/>
-      <Payment/>
-      <EditMenu/>
-      <AdminPage/>
-      <Report/>
-      <Contact/>
-      <Navigation/>
+       {/* <Login/> */}
+       {/* <Resevation/> */}
+      {/* //  <Register/> */}
+      {/* //  <Staff/> */}
+      {/* <OnlineOrder/> */}
+      {/* <CustomerFeedback/> */}
+      {/* <Payment/> */}
+      {/* <EditMenu/> */}
+      {/* <AdminPage/> */}
+      {/* <Report/> */}
+      {/* <Contact/> */}
+      {/* <Navigation/> */}
     </div>
   );
 }
